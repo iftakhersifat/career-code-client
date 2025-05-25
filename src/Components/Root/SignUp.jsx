4aa004fe01569fb1eaa9f04a4200a90fe064a09c
iftakhersifat <hossensifat615@gmail.com>
2025-05-26 01:05:04 +0600

    added lottie animation

--- src/Components/Root/SignUp.jsx
+++ src/Components/Root/SignUp.jsx
@@ -1,29 +1,56 @@
+import Lottie from 'lottie-react';
 import React from 'react';
 import { Link } from 'react-router';
+import register from "../../lottie/register.json";
 
 const SignUp = () => {
     return (
-        <div className="hero  min-h-screen ">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="card md:w-[500px] w-[350px] border border-green-200 bg-gray-200 shrink-0 shadow-md">
-        <div className="card-body">
-        <form className="fieldset">
+        <div className="hero min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
 
-        <label className="label">Email</label>
-        <input type="email" className="input w-full" name='email' placeholder="Email" />
+                {/* Lottie Animation */}
+                <div className="w-[300px] md:w-[400px]">
+                    <Lottie
+                        animationData={register}
+                        loop={true}
+                        autoplay={true}
+                        style={{ width: '100%', height: '100%' }}
+                    />
+                </div>
 
-          <label className="label">Password</label>
-          <input type="password" className="input w-full" name='password' placeholder="Password" />
+                {/* Form Section */}
+                <div className="card md:w-[500px] w-[350px] border border-green-200 bg-gray-200 shrink-0 shadow-md">
+                    <div className="card-body">
+                        <form className="fieldset">
 
-          <div><a className="link link-hover">Forgot password?</a></div>
-          <button className="btn bg-green-500 text-white mt-4">Sign Up</button>
+                            <label className="label">Email</label>
+                            <input
+                                type="email"
+                                className="input w-full"
+                                name="email"
+                                placeholder="Email"
+                            />
 
-          <Link to="/login">Do you have an account? <span className='text-red-500 underline'>Login</span></Link>
-        </form>
-      </div>
-    </div>
-  </div>
-</div>
+                            <label className="label">Password</label>
+                            <input
+                                type="password"
+                                className="input w-full"
+                                name="password"
+                                placeholder="Password"
+                            />
+
+                            <div><a className="link link-hover">Forgot password?</a></div>
+
+                            <button className="btn bg-green-500 text-white mt-4">Sign Up</button>
+
+                            <Link to="/login" className="mt-3 block">
+                                Do you have an account? <span className="text-red-500 underline">Login</span>
+                            </Link>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
