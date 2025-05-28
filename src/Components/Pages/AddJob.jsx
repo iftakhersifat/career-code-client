d19802e34b6d9c8dfc3b60a27ea52f240cc3d14d
iftakhersifat <hossensifat615@gmail.com>
2025-05-28 21:36:18 +0600

    added form for addJob

--- src/Components/Pages/AddJob.jsx
+++ src/Components/Pages/AddJob.jsx
@@ -41,6 +41,60 @@ const AddJob = () => {
   <option>Finance</option>
 </select>
 </fieldset>
+{/* application deadline */}
+<fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
+  <legend className="fieldset-legend">Application Deadline</legend>
+
+  <input type="date" className="input" />
+
+</fieldset>
+
+{/* salary range */}
+<fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+  <legend className="fieldset-legend text-lg font-semibold mb-2">Salary Range</legend>
+
+  <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
+    {/* Minimum Salary */}
+    <div>
+      <label className="label font-medium">Minimum Salary</label>
+      <input 
+        type="text" 
+        name='minimum' 
+        placeholder="Enter minimum salary" 
+        className="input input-bordered w-full" 
+        required 
+      />
+    </div>
+
+    {/* Maximum Salary */}
+    <div>
+      <label className="label font-medium">Maximum Salary</label>
+      <input 
+        type="text" 
+        name='maximum' 
+        placeholder="Enter maximum salary" 
+        className="input input-bordered w-full" 
+      />
+    </div>
+
+    {/* Currency Selection */}
+    <div>
+      <label className="label font-medium">Currency</label>
+      <select 
+        defaultValue="" 
+        className="select select-bordered w-full" 
+        name="currency"
+        required
+      >
+        <option disabled value="">Select a Currency</option>
+        <option>BDT</option>
+        <option>USD</option>
+        <option>EU</option>
+      </select>
+    </div>
+  </div>
+</fieldset>
+
 
 
         </div>
