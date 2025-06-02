e8526bf908a29d7ec9746f263d689efb9e71462f
iftakhersifat <hossensifat615@gmail.com>
2025-06-02 20:41:58 +0600

    added error for axios instance

--- src/Components/AxiosInstance/AxiosInstance.jsx
+++ src/Components/AxiosInstance/AxiosInstance.jsx
@@ -7,12 +7,28 @@ const axiosInstance =axios.create({
 })
 
 const AxiosInstance = () => {
-    const {user} =useAuth();
+    const {user, logOut} =useAuth();
 
     axiosInstance.interceptors.request.use(config=>{
         config.headers.authorization = `Bearer ${user.accessToken}`
         return config
     })
+    // Add a response interceptor
+    axiosInstance.interceptors.response.use(response => {
+
+    return response;
+  }, error =>{
+    if(error.status === 401 || error.status === 403){
+        logOut()
+        .then(()=>{
+            console.log("sign out for 401 status")
+        }).catch(error=>{
+            console.log(error)
+        })
+    }
+    return Promise.reject(error)
+
+  })
 
     return axiosInstance;
 };
